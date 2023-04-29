10a0866e126fe53547c458287007542fc202fb7d
HamidReza Farahani <h.farahani>
2023-04-29 15:27:19 +0330

    Initial commit 1402/02/09

---
+++ app/src/main/java/com/example/tmdbsampleapp/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.tmdbsampleapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ *   66-92-110-530-118-128
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
