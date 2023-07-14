eb881b4883c4331628f6b3fc32ea5ef4f1438553
HamidReza Farahani <h.farahani>
2023-07-14 19:41:41 +0430

    using bottom navigation in activity

--- app/src/main/java/com/example/tmdbsampleapp/MainActivity.kt
+++ app/src/main/java/com/example/tmdbsampleapp/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.tmdbsampleapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.tmdbsampleapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -19,5 +23,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navView = binding.bottomNavigation
+
+        val navController = findNavController(R.id.fragmentContainerView)
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.moviesFragment, R.id.searchFragment)
+        )
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
