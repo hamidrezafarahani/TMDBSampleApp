83cc7ef6d89a7f91b8dac6d2f8d921d6e6281604
HamidReza Farahani <h.farahani>
2023-07-15 16:40:20 +0430

    end impl of search fragment, using search view.

--- app/src/main/java/com/example/tmdbsampleapp/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/tmdbsampleapp/ui/search/SearchFragment.kt
@@ -1,23 +1,108 @@
 package com.example.tmdbsampleapp.ui.search
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
+import androidx.appcompat.widget.SearchView
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.recyclerview.widget.RecyclerView
 import com.example.tmdbsampleapp.R
 import com.example.tmdbsampleapp.binding.dataBindings
 import com.example.tmdbsampleapp.databinding.FragmentSearchBinding
 import com.example.tmdbsampleapp.extensions.autoCleared
+import com.example.tmdbsampleapp.extensions.with
+import com.example.tmdbsampleapp.network.apierror.GlobalApiErrorHandler
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class SearchFragment : Fragment(R.layout.fragment_search) {
 
     private val binding by dataBindings(FragmentSearchBinding::bind)
     private val viewModel by viewModels<SearchViewModel>()
-    private val adapter by autoCleared<SearchMoviesAdapter>()
+    private var adapter by autoCleared<SearchMoviesAdapter>()
+
+    @Inject
+    lateinit var apiErrorHandler: GlobalApiErrorHandler
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        adapter = SearchMoviesAdapter {
+            Snackbar.make(view, it.title, Snackbar.LENGTH_LONG).show()
+        }
+        binding.rvListMovies.adapter = adapter
+        binding.rvListMovies.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                if (!recyclerView.canScrollVertically(1)) {
+                    viewModel.loadNextPage()
+                }
+            }
+        })
+
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(object : MenuProvider {
+
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.search_menu, menu)
+                val searchViewItem = menu.findItem(R.id.search_view)
+                val searchView = searchViewItem.actionView as SearchView
+                searchView.apply {
+
+                    var queryText: String? = null
+                    setOnSearchClickListener {
+                        setQuery(queryText, false)
+                    }
+
+                    setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                        override fun onQueryTextSubmit(query: String?): Boolean {
+                            queryText = query
+                            query?.let { binding.search(it) }
+                            return true
+                        }
+
+                        override fun onQueryTextChange(newText: String?): Boolean {
+                            return false
+                        }
+                    })
+                }
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                return when (menuItem.itemId) {
+                    R.id.search_view -> true
+                    R.id.clear_search_query -> with(viewModel) {
+                        cachedMovies.clear()
+                        adapter.submitList(cachedMovies.toList())
+                        true
+                    }
+
+                    else -> false
+                }
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
+    }
+
+    private fun FragmentSearchBinding.search(query: String) = with(viewModel) {
+        search(query).with(viewLifecycleOwner).callbacks {
+            onLoading {
+
+            }
+            onSuccess {
+                cachedMovies.addAll(it)
+                adapter.submitList(cachedMovies.toList())
+            }
+            onFailed {
+                apiErrorHandler.handleError(root, it)
+            }
+        }
     }
 }
--- app/src/main/java/com/example/tmdbsampleapp/ui/search/SearchViewModel.kt
+++ app/src/main/java/com/example/tmdbsampleapp/ui/search/SearchViewModel.kt
@@ -1,12 +1,36 @@
 package com.example.tmdbsampleapp.ui.search
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.switchMap
+import com.example.tmdbsampleapp.data.remote.dto.Movie
+import com.example.tmdbsampleapp.domain.GetMoviesUseCase
+import com.example.tmdbsampleapp.network.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class SearchViewModel @Inject constructor(
+    private val getMovies: GetMoviesUseCase,
     private val savedStateHandle: SavedStateHandle
 ) : ViewModel() {
+
+    private val currentPage = MutableLiveData(1)
+    val cachedMovies = mutableListOf<Movie>()
+
+    fun search(query: String): LiveData<Resource<List<Movie>>> {
+        return currentPage.switchMap {
+            getMovies(query, it)
+        }
+    }
+
+    fun loadFirstPage() {
+        currentPage.value = 1
+    }
+
+    fun loadNextPage() {
+        currentPage.value = currentPage.value?.plus(1)
+    }
 }
