3e7a4c55339662d9cf81e237bae699c1abbdc4c7
HamidReza Farahani <h.farahani>
2023-07-19 16:44:58 +0430

    create plugins object to get plugins in top level build.gradle.kts file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version Versions.ANDROID_GRADLE_PLUGIN apply false
-    id("com.android.library") version Versions.ANDROID_GRADLE_PLUGIN apply false
-    id("org.jetbrains.kotlin.android") version Versions.KOTLIN_ANDROID apply false
-    id("androidx.navigation.safeargs.kotlin") version Versions.NAVIGATION apply false
-    id("com.google.dagger.hilt.android") version Versions.HILT apply false
+    id(Plugins.ANDROID_APPLICATION) version Versions.ANDROID_GRADLE_PLUGIN apply false
+    id(Plugins.ANDROID_LIBRARY) version Versions.ANDROID_GRADLE_PLUGIN apply false
+    id(Plugins.JETBRAINS_KOTILN_ANDROID) version Versions.KOTLIN_ANDROID apply false
+    id(Plugins.NAVIGATION_SAFE_ARGS) version Versions.NAVIGATION apply false
+    id(Plugins.DAGGER_HILT_ANDROID) version Versions.HILT apply false
 }
---
+++ buildSrc/src/main/java/Plugins.kt
@@ -0,0 +1,8 @@
+object Plugins {
+
+    const val ANDROID_APPLICATION = "com.android.application"
+    const val ANDROID_LIBRARY = "com.android.library"
+    const val JETBRAINS_KOTILN_ANDROID = "org.jetbrains.kotlin.android"
+    const val NAVIGATION_SAFE_ARGS = "androidx.navigation.safeargs.kotlin"
+    const val DAGGER_HILT_ANDROID = "com.google.dagger.hilt.android"
+}
